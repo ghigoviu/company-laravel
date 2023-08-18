57444dc5734bd25687dc26ff84b74b3e51085932
Ghigoviu <you@example.com>
2023-08-18 12:50:34 -0600

    add feat Department modal window

--- resources/js/Pages/Departments/Create.vue
+++ resources/js/Pages/Departments/Create.vue
@@ -1,10 +1,10 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
+import { Head, useForm } from '@inertiajs/vue3';
 
 const form = useForm({
     name: ''
@@ -21,9 +21,16 @@ const form = useForm({
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <form @submit.prevent="$event => form.post(route('departments.store'))" >
-                    
+                <div class="p-5 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <form @submit.prevent="$event => form.post(route('departments.store'))" 
+                        class="mt-6 space-y-6 max-w-xl" >
+                        <InputLabel for="name" value="Department"></InputLabel>
+                        <TextInput id="name" v-model="form.name" autofocus required 
+                        type="text" class="mt-1 block w-full"></TextInput>
+                        <InputError :message="form.errors.name" class="mt-2"></InputError>
+                        <PrimaryButton :disabled="form.processing">
+                            <i class="fa-solid fa-save"></i>Guardar
+                        </PrimaryButton>
                     </form>
                 </div>
             </div>
